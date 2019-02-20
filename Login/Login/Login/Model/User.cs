f990d600ba61f9ef20abf902e890dddf782827f0
chriscrn96 <christiantmpr181296@gmail.com>
2019-02-19 23:47:32 -0500

    Version 1.0.1

---
+++ Login/Login/Login/Model/User.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using System.Threading.Tasks;
+using SQLite;
+
+namespace Login.Model
+{
+    [SQLite.Net.Attributes.Table("user")]
+    public class User
+    {
+        [SQLite.Net.Attributes.PrimaryKey, SQLite.Net.Attributes.AutoIncrement]
+        public int Id { get; set; }
+
+        [SQLite.Net.Attributes.MaxLength(100), SQLite.Net.Attributes.Unique]
+        public String Username { get; set; }
+
+        [SQLite.Net.Attributes.MaxLength(100), SQLite.Net.Attributes.Unique]
+        public String Email { get; set; }
+
+        [SQLite.Net.Attributes.MaxLength(70), SQLite.Net.Attributes.Unique]
+        public String Password{ get; set; }
+
+    }
+}
---
+++ Login/Login/Login/Model/UserRepository.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using System.Threading.Tasks;
+using SQLite;
+using SQLite.Net;
+using SQLiteConnection = SQLite.SQLiteConnection;
+
+namespace Login.Model
+{
+    public class UserRepository
+    {
+        private SQLiteConnection con;
+
+        private static UserRepository instancia;
+
+        public static UserRepository Instancia
+
+        {
+            get
+            {
+                if (instancia == null)
+                    throw new Exception("Debe llamar al inicializador");
+                return instancia;
+
+            }
+
+        }
+
+        public static void Inicializador(String filename)
+        {
+            if (filename == null)
+                throw new ArgumentNullException();
+            if (instancia != null)
+                instancia.con.Close();
+
+
+
+            instancia = new UserRepository(filename);
+
+        }
+
+        private UserRepository(String dbPath)
+        {
+
+            con = new SQLiteConnection(dbPath);
+            con.CreateTable<User>();
+
+
+        }
+        public String EstadoMensaje;
+        public int AddNewUser(string username, string email, string password)
+        {
+            int result = 0;
+            try
+            {
+                result = con.Insert(new User() {
+                    Username = username,
+                    Email = email,
+                    Password = password
+                });
+
+                EstadoMensaje = string.Format("Cantidad de filas afectadas {0}", result);
+            }
+            catch (Exception e)
+            {
+                EstadoMensaje = e.Message;
+
+            } return result;
+
+        }
+        public IEnumerable<User> GetAllUsers()
+        {
+            try
+            {
+                return con.Table<User>();
+            }
+            catch (Exception e)
+            {
+                EstadoMensaje = e.Message;
+               
+            }
+
+            return Enumerable.Empty<User>();
+        }
+    }
+}
